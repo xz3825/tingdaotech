bc80477088d528ed7e0abe3b90b5716ac7f32725
xz3825 <xz3825@nyu.edu>
2026-07-07 14:42:55 +0800

    更新网站内容

--- js/main.js
+++ js/main.js
@@ -3,47 +3,28 @@
 
   var toggle = document.querySelector(".menu-toggle");
   var navList = document.querySelector(".nav-list");
-  var navLinks = document.querySelectorAll(".nav-list a");
-  var sections = document.querySelectorAll("section[id]");
 
   if (toggle && navList) {
     toggle.addEventListener("click", function () {
       navList.classList.toggle("open");
     });
+
+    document.addEventListener("click", function (e) {
+      if (!toggle.contains(e.target) && !navList.contains(e.target)) {
+        navList.classList.remove("open");
+      }
+    });
   }
 
-  function closeMenu() {
-    if (navList) {
-      navList.classList.remove("open");
-    }
-  }
+  var navLinks = document.querySelectorAll(".nav-list a");
+  var currentPage = window.location.pathname.split("/").pop() || "index.html";
 
   for (var i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener("click", closeMenu);
-  }
-
-  function setActiveNav() {
-    var scrollY = window.scrollY + 80;
-
-    for (var j = 0; j < sections.length; j++) {
-      var section = sections[j];
-      var top = section.offsetTop;
-      var height = section.offsetHeight;
-      var id = section.getAttribute("id");
-
-      if (scrollY >= top && scrollY < top + height) {
-        for (var k = 0; k < navLinks.length; k++) {
-          navLinks[k].classList.remove("active");
-          if (navLinks[k].getAttribute("href") === "#" + id) {
-            navLinks[k].classList.add("active");
-          }
-        }
-      }
+    var href = navLinks[i].getAttribute("href");
+    if (href === currentPage || (currentPage === "" && href === "index.html")) {
+      navLinks[i].classList.add("active");
     }
   }
-
-  window.addEventListener("scroll", setActiveNav);
-  setActiveNav();
 
   var contactForm = document.getElementById("contact-form");
   if (contactForm) {
